1085f92e22a4256e734e78450012c0a545a0febc
Amara <amarahenry@icloud.com>
2023-08-31 23:51:50 -0500

    completed climate_starter doc

--- Hawaiian Climate/app.py
+++ Hawaiian Climate/app.py
@@ -1,22 +1,31 @@
 # Import the dependencies.
 
+import numpy as np
+import pandas as pd
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
